0e7e61c8dd4c63331cb2eec5d73a0c0b04710fe1
Moisés Emanuel <113472007+moimanu@users.noreply.github.com>
2024-08-25 00:57:39 -0300

    Update planoCartesiano.java

--- java/testesPessoais/planoCartesiano.java
+++ java/testesPessoais/planoCartesiano.java
@@ -2,40 +2,40 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        Scanner t = new Scanner(System.in);
-        
-        int A, B, C, x, y, size = 30;
-        
-        System.out.println("Digite os coeficientes da função -> f(x)=Ax²+Bx+C");
-	System.out.print("A: ");
-	A = t.nextInt();
+	public static void main (String[] args) {
+		Scanner t = new Scanner(System.in);
 
-	System.out.print("B: ");
-	B = t.nextInt();
+		int A, B, C, x, y, size = 30;
 
-	System.out.print("C: ");
-	C = t.nextInt();
-    
-        for(y = size; y >= -size; y--){
-	    for(x = -size; x <= size; x++){
-		if(y == 0 && x == size){
-		    System.out.print("> X");
-		} else if(y == size && x == 0){
-		    System.out.print(" ^ ");
-		} else if (y == size && x == 1){
-		    System.out.print(" Y ");
-		} else if(y == (A*(x*x)) + (B*x) + C){
-		    System.out.print(" o ");
-		} else if(x == 0){
-		    System.out.print(" • ");
-		} else if(y == 0){
-		    System.out.print(" • ");
-		} else {
-		    System.out.print("   ");
+		System.out.println("Digite os coeficientes da função -> f(x)=Ax²+Bx+C");
+		System.out.print("A: ");
+		A = t.nextInt();
+
+		System.out.print("B: ");
+		B = t.nextInt();
+
+		System.out.print("C: ");
+		C = t.nextInt();
+
+		for(y = size; y >= -size; y--){
+			for(x = -size; x <= size; x++){
+				if(y == 0 && x == size){
+					System.out.print("> X");
+				} else if(y == size && x == 0){
+					System.out.print(" ^ ");
+				} else if (y == size && x == 1){
+					System.out.print(" Y ");
+				} else if(y == (A*(x*x)) + (B*x) + C){
+					System.out.print(" o ");
+				} else if(x == 0){
+					System.out.print(" • ");
+				} else if(y == 0){
+					System.out.print(" • ");
+				} else {
+					System.out.print("   ");
+				}
+			}
+			System.out.print("\n");
 		}
-	    }
-	    System.out.print("\n");
 	}
-    }
 }
