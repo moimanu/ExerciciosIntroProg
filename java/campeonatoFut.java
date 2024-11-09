6d9b345e45b5de6ffe4bf2f533c1330fda308fbf
Moisés Emanuel <113472007+moimanu@users.noreply.github.com>
2024-11-08 22:22:35 -0300

    Create campeonatoFut.java

---
+++ java/campeonatoFut.java
@@ -0,0 +1,35 @@
+package campeonatofut;
+
+public class CampeonatoFut {
+    
+    public static void menu(TimesFut [] vetor, int qntdTimesAtual, boolean comecouCampeonato){
+        
+        if(qntdTimesAtual <= 2){
+            //CADASTRAR TIME
+        } else {
+            if (comecouCampeonato) {
+                //CADASTRAR TIME
+                //CADASTRAR PARTIDA
+            } else {
+                if (!comecouCampeonato) {
+                //CADASTRAR TIME
+                //CADASTRAR PARTIDA
+                //VER LIB
+                //VER ZONA
+            }
+            }
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        TimesFut tabelaTimes [] = new TimesFut [20];
+        int qntdTimes = 0; 
+        boolean comecouCampeonato = false;
+        
+        menu(tabelaTimes, qntdTimes, comecouCampeonato);
+        
+    }
+    
+}
